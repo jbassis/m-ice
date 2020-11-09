fcf7959d71868f7da848919f3918026eb1b826f4
Jeremy N. Bassis <jbassis@umich.edu>
2020-11-09 16:57:13 -0500

    Script to do calibration with m-ice

---
+++ src/main/calibrate.py
@@ -0,0 +1,490 @@
+"""
+Test uni-axial compression without gravity
+"""
+from importlib import reload
+
+
+import meshModel
+reload(meshModel)
+from meshModel import *
+
+import material
+reload(material)
+from material import *
+
+from scipy.special import erfc
+
+
+import tempModel
+reload(tempModel)
+from tempModel import *
+
+import stokes2Dve
+reload(stokes2Dve)
+from stokes2Dve import *
+
+from leopart import (
+    particles,
+    RandomRectangle,
+    l2projection,
+    advect_rk3,
+    assign_particle_values,
+    AddDelete,
+    RandomCell
+)
+
+from fenics import plot as fplot
+
+import time
+
+import os
+
+import pylab as plt
+
+import logging
+logging.getLogger('FFC').setLevel(logging.ERROR)
+logging.getLogger('UFL').setLevel(logging.ERROR)
+parameters['ghost_mode']='shared_facet'
+
+set_log_level(50)
+set_log_active(False)
+
+# Turn on plastic failure
+plastic = True
+
+# Thick cliff
+xyield_min = 0.0
+
+# Surface and bottom temperatures
+Ts = -20.0
+Tb = -20.0
+
+melange = False
+buttressing = 1e-32
+buttressing_height_max = 25.0 # In meters
+buttressing_height_min = 55.0 # In meters
+
+
+# Set length of domain and water depth
+water_depth =-100.0
+ice_thick = 100.0
+length= ice_thick
+#water_depth = ice_thick*910.0/1020 - Hab
+
+
+
+# Set mesh resolution and estimate approximate number of points in x/z dir
+dz = (ice_thick/13.333333333/2)
+Nx = int(length/dz)
+Nz = int(ice_thick/dz)
+
+# Define geometry of domain
+surf_slope =  0.0
+bed_slope =   0.0
+strain_rate = 1e-16
+left_vel =    length*strain_rate/2#/material.secpera*material.time_factor
+right_vel =  -length*strain_rate/2#/material.secpera*material.time_factor
+
+
+L = length+ice_thick*0
+bump_width = ice_thick
+bump_height =  0.25*ice_thick*0
+
+def bot_fun(x):
+   b = -water_depth + bed_slope*(length-x)
+   return b
+
+def bed_fun(x):
+   b = -water_depth + bed_slope*(length-x) + bump_height*exp(-((L-x)/bump_width)**2)
+   return b
+
+def bed_fun2(x):
+   b = -water_depth + bed_slope*(length-x) + bump_height*exp(-((L-x)/bump_width)**2)
+   return b-200.0
+
+def bed_fun_np(x):
+   b = -water_depth + bed_slope*(length-x) + bump_height*np.exp(-((L-x)/bump_width)**2)
+   return b
+
+def ice_thick_fun(x):
+    return ice_thick + (surf_slope-bed_slope)*(L-x)
+
+def surf_fun(x):
+   s = -water_depth + ice_thick + (surf_slope)*(L-x)#-  notch_slope*(L+notch_length-x)*(x<(L+notch_length))*(x>(L)) + notch_slope*(L-x-notch_length)*(x>(L-notch_length))*(x<=(L))
+   return s
+
+# Initialize mesh
+start = time.time()
+mesh = MeshModelPoly(surf_fun,bed_fun_np,bed_fun_np,Nx=Nx,Nz=Nz,length=length,dz=dz)
+mesh.length = length*1.1
+mesh.bed_fun = bed_fun2
+print('Time to make mesh and basis functions',time.time()-start)
+
+#_____________________________________________
+# Create particles defined on mesh
+#xm,zm = mesh.get_coords();xmin = np.min(xm); xmax=np.max(xm);ymin = np.min(zm); ymax=np.max(zm)
+#xp = RandomRectangle(Point(xmin, ymin), Point(xmax, ymax)).generate([Nx*10, Nz*10])
+p_min = 16
+p_max = 32
+
+#p_min = 16
+#p_max = 32
+print('Generating particles')
+gen = RandomCell(mesh.mesh)
+xp = gen.generate(p_min)
+print('Done generating particles')
+#_____________________________________________
+# Define function space for strain and temperature function
+Vdg = FunctionSpace(mesh.mesh, 'DG',1)
+
+print('Initializing function spaces')
+strain_mesh, temp_mesh, epsII_mesh = Function(Vdg), Function(Vdg), Function(Vdg)
+
+# Initial conditions for strain and temp
+strain_fun = Expression("0.0", degree=1)
+temp_fun = temp_init(Ts,Tb,surf_fun, bed_fun,degree=1)
+
+# Create functions defined on mesh with initial values
+strain_mesh.assign(strain_init)
+temp_mesh.assign(interpolate(temp_fun,Vdg))
+epsII_mesh.assign(strain_init)
+
+# Particle values at nodes
+pstrain = assign_particle_values(xp, strain_fun)
+pepsII = assign_particle_values(xp, strain_fun)
+ptemp = assign_particle_values(xp, temp_fun)
+print('Done initializing function spaces')
+
+
+# Now we initialize the particle class
+print('Creating particles')
+p = particles(xp, [pstrain,ptemp,pepsII], mesh.mesh)
+print('Done particles')
+
+
+# Make sure we have enough particles per cell
+#AD = AddDelete(p, p_min, p_max, [strain_mesh, temp_mesh,epsII_mesh]) # Sweep over mesh to delete/insert particles
+#AD.do_sweep()
+
+(xp , pstrain , ptemp, pepsII) = (p. return_property(mesh , 0) ,
+    p. return_property(mesh , 1) ,
+    p. return_property(mesh , 2),
+    p. return_property(mesh , 3))
+
+#p.change_property(1e-2*np.random.rand(len(pstrain)),1)
+#_____________________________________________
+# Initialize temperature model
+print('Initializing temperature')
+Tmodel = tempModel(mesh.mesh,Tb=Tb,Ts=Ts)
+x = SpatialCoordinate(mesh.mesh)
+zb = bot_fun(x[0])
+zs = surf_fun(x[0])
+Tmodel.set_mesh(mesh.mesh)
+Tmodel.set_temp(x,surf_fun,bot_fun,bed_fun)
+print('Done initializing temperature')
+
+#_____________________________________________
+# Viscosity and material properties
+# Define the rheology and make sure we set some of the properties
+glenVisc = glenFlow(grain_size=1e-3)
+glenVisc.yield_strength=0.75e6#-12.5e3
+glenVisc.visc_min = 5e9
+glenVisc.visc_max = 1e30
+glenVisc.plastic = plastic
+glenVisc.yield_min = 20e3
+glenVisc.crit_strain = 0.1
+glenVisc.mu = 0.0
+
+#_____________________________________________
+# Viscosity and material properties
+# Set inflow velocity of the domain
+
+#_____________________________________________
+# Define our model and some parameters
+model = Stokes2D(mesh,glenVisc,left_vel=left_vel,right_vel=right_vel)
+model.tempModel=Tmodel
+model.tolerance = 1e-5
+model.u_k = None
+model.maxit = 25
+model.maxit_local = 100
+model.local_err_min = 1.0
+model.tracers = particles
+
+model.calving_front = True # This applies a stress boundary condition to the right edge
+model.alpha = 1.0  # Not used anymore, legacy from when we used overrelaxation
+model.water_drag = 1e5 # Quadratic drag term associated with water drag
+# Add lateral drag to model
+B = 0.75e8
+width = 10e3
+model.lateral_drag = 2*(4)**(1.0/3)*B/(width**(4.0/3))*0
+model.bed_yield_stregth = 400e3
+model.friction = 1e-32#/(surf_fun(0)-bed_fun(0))/model.rho_i/model.g # Friction coefficient
+model.m = 1.0/3.0 # Friction exponent
+
+model.left_wall = 0.0
+model.right_wall = length
+
+model.alpha = 1.0
+
+#_____________________________________________
+# Maximum time step
+time_step_secs = 86400.0/3# Time step in seconds
+
+time_step = time_step_secs/material.time_factor # Convert time step to unit we are using
+
+#_____________________________________________
+# Start loop for simulation
+# Initialize time vector to zero
+t = 0.0
+it_type = 'Picard'
+
+max_length = length# Regrid if length exceeds this value
+min_length = length
+model.mesh.length = max_length # Set this as the max length of the mesh--doesn't actually do anything
+save_files = True # Set to True if we want to save output files
+fname_dir = 'data/calibrate/'
+fname_base = fname_dir + 'calibration_1_'+str((left_vel-right_vel)/length)+'_T_'+str(Tb)+'/'
+
+if not os.path.exists(fname_base):
+    os.makedirs(fname_base)
+
+
+if save_files==True:
+    import shutil
+    shutil.copy2('calibrate.py', fname_base+'calibrate.py')
+    shutil.copy2('stokes2Dve.py', fname_base+'stokes2Dve.py')
+
+
+CFL = 1.0
+model.u_k = None
+tau = 0.1*60*60/(60*60*24*365.24) # Relaxation time for upstream plastic strain
+i =0
+model.strain = Function(model.mesh.Q)
+L2_strain = []
+tlist = []
+model.method = 1
+model.buttressing = buttressing # Apply buttressing force (in Pa) in x-direction to portion of ice under water???
+model.buttressing_height_max = buttressing_height_max # Apply buttressing force (in Pa) in x-direction to portion of ice under water???
+model.buttressing_height_min = buttressing_height_min # Apply buttressing force (in Pa) in x-direction to portion of ice under water???
+model.buttressing = 1e-32
+model.right_wall = length
+outF = open(fname_base+"calibrate3.txt", "w")
+strain_rate = (left_vel-right_vel)/length
+s = []
+f1 = []
+f2 = []
+model.g =1e-256
+
+
+
+#strain_rate =1e-16
+#strain_rate_accel = 1e-2
+accumulated_strain = 0.0
+strain_rate_vec = []
+#s.append(0.0)
+#f1.append(0.0)
+#f2.append(0.0)
+for i in range(i,1000):
+   #model.left_vel =    length*strain_rate/2#/material.secpera*material.time_factor
+   #model.right_vel =  -length*strain_rate/2#/material.secpera*material.time_factor
+
+   #L2_strain.append(assemble(model.strain*dx(model.mesh.mesh))/assemble(Constant(1.0)*dx(model.mesh.mesh)))
+   #L2_strain.append(assemble(model.strain*dx(model.mesh.mesh)))
+   #tlist.append(t)
+   # First need to interpolate tracer quantities to nodes
+   #node_vars = particles.tracers_to_nodes()
+
+   # For first time step, we use a small time step.  After that, we use the CFL criterion
+   Q0 = FunctionSpace(model.mesh.mesh, "DG", 0)
+   if i==0:
+       time_step = time_step_secs/material.time_factor
+   else:
+       Q0 = FunctionSpace(model.mesh.mesh, "DG", 0)
+       quality=np.min(MeshQuality.radius_ratios(model.mesh.mesh).array())
+       time_step = CFL*np.min(project(CellDiameter(model.mesh.mesh)/sqrt(inner(u,u)),Q0).compute_vertex_values())
+       time_step = np.minimum(time_step_secs/material.time_factor,time_step)
+
+
+   print('Time step',time_step)
+   u,pres = model.solve(p,dt=time_step,tolerance=model.tolerance)
+   sigma = 2 * model.eta * 0.5*(nabla_grad(u) + nabla_grad(u).T) - pres*Identity(2)
+   n = FacetNormal(model.mesh.mesh)
+   Tn = dot(dot(sigma,n),n)
+   Tn = dot(sigma,n) # Normal stress
+   flux_1 = assemble(Tn[0]*model.DS(3))/assemble(dot(n,n)*model.DS(3))
+   flux_2 = assemble(Tn[0]*model.DS(4))/assemble(dot(n,n)*model.DS(4))
+   #flux_3 = assemble(Tn*model.DS(1))/assemble(dot(n,n)*model.DS(1))
+   flux_3 = 0.0
+   flux_4 = assemble(Tn[1]*model.DS(5))/assemble(dot(n,n)*model.DS(5))
+   print('Flux 4',flux_4/1e6)
+   eta_visc_2 = assemble(model.eta_visc*model.DS(4))/assemble(dot(n,n)*model.DS(4))
+   eta_visc_1 = assemble(model.eta_visc*model.DS(3))/assemble(dot(n,n)*model.DS(3))
+   #eta_visc=assemble(model.eta_visc*dx)/assemble(interpolate(Constant(1.0),Q0)*dx)
+   sigma1=(flux_1-flux_2)/1e6
+   sigma2=(flux_3-flux_4)/1e6
+   sigma11 = -0.5*(sigma1+sigma2)
+   sigma11 = sigma1
+   sigma12 = 0.5*(sigma1-sigma2)
+
+   #sigma11=-(flux_1-flux_2)/1e6
+   #sigma12=(flux_3-flux_4)/1e6
+
+   tau_xx = 2 * model.eta * 0.5*(nabla_grad(u) + nabla_grad(u).T)
+   epsII_visc=np.abs(assemble(sqrt(tau_xx[0,0]**2+tau_xx[0,1]**2)/model.eta_visc*dx)/assemble(interpolate(Constant(1.0),Q0)*dx))
+   strain_rate = (model.left_vel-model.right_vel)/(model.right_wall-model.left_wall)
+   accumulated_strain = accumulated_strain + np.maximum(strain_rate-epsII_visc,0.0)*time_step
+   line = str(accumulated_strain)+','+str(strain_rate/secpera)+','+str(sigma11)+','+str(sigma12)+','+str(np.max(pstrain))+'\n'
+   outF.write(line)
+   s.append(accumulated_strain)
+   f1.append(sigma11)
+   f2.append(sigma12)
+   strain_rate_vec.append(strain_rate/secpera)
+   #model.right_wall = model.right_wall + model.right_vel*time_step
+   #model.left_wall = model.left_wall + model.left_vel*time_step
+   # Do a little bit of accounting to make sure that our time step doesn't violate the CFL criterion
+   ux, uz = model.get_velocity();speed = np.sqrt(ux**2+uz**2)
+   Q0 = FunctionSpace(model.mesh.mesh, "DG", 0)
+   time_step_CFL = CFL*np.min(project(CellDiameter(model.mesh.mesh)/sqrt(inner(u,u)),Q0).compute_vertex_values())
+   #print('Time step CFL',time_step_CFL)
+   #if time_step_CFL<0.5*time_step:
+    #   time_step=time_step_CFL
+     #  u,pres = model.solve(node_vars,dt=time_step,tolerance=model.tolerance);#model.u_k = None
+   #xm,zm = particles.get_coords()
+
+   #particles.tracers['Strain'][xm<1e3]=particles.tracers['Strain'][xm<1e3]/(1.0+time_step/tau)
+
+   # Decide if we need to remesh
+   x,z = model.mesh.get_coords()
+
+   #if model.mesh.mesh.hmax()/model.mesh.mesh.hmin() > 10.0:
+    #   remesh_elastic=False
+     #  model.mesh.length=min_length
+
+   quality=np.min(MeshQuality.radius_ratios(model.mesh.mesh).array())
+   remesh_elastic = True
+   if quality<0.1:
+       remesh_elastic=False
+       model.mesh.length=min_length
+
+   #remesh_elastic = False
+   print(remesh_elastic)
+   if np.mod(i,10)==0:
+      #particles.tracers['Strain'][xm<3.5e3]=0.0
+      if save_files == True:
+          #uxm = particles.tracers['ux']
+          #uzm = particles.tracers['uz']
+          (xp , pstrain , ptemp, pepsII) = (p. return_property(mesh , 0) ,
+            p. return_property(mesh , 1) ,
+            p. return_property(mesh , 2),
+            p. return_property(mesh , 3))
+
+          #eta_visc_m = model.tracers.nodes_to_tracers(model.eta_visc)
+          #eta_plas_m = model.tracers.nodes_to_tracers(model.eta_plas)
+          #eta_m = model.tracers.nodes_to_tracers(model.eta)
+          fname =fname_base + 'glacier_cliff_'+str(i).zfill(3)+'.npz'
+          print(fname)
+          np.savez(fname, t=t, xm=xp[:,0],zm=xp[:,1],speed=speed,ux=ux,uz=uz,strain=pstrain,
+               epsII=pepsII/material.time_factor,temp=ptemp)
+
+          mesh_file_name =fname_base + 'glacier_cliff_'+str(i).zfill(3)+'.xml'
+          mesh_file = File(mesh_file_name)
+          mesh_file << model.mesh.mesh
+          temp_file_name = fname_base + 'temp_'+str(i).zfill(3)+'.hdf'
+          temp_file=HDF5File(model.mesh.mesh.mpi_comm(), temp_file_name, 'w')
+          temp_file.write(u,'u')
+          temp_file.write(model.strain,'strain')
+          temp_file.close()
+
+
+   # Update all quantities (need to update this to simplify it and use RK4 if specified)
+   time_step = model.update(u,time_step,p,remesh_elastic=remesh_elastic)
+   yr = int(np.mod(t,365))
+   #day = round((t - yr)*365,1)
+   day = (t - yr)*365
+   hr,day = np.modf(day)
+   hr = round(hr*24,0)
+   title_str = 'Time: '+str(yr).zfill(1)+ 'a '+str(int(day)).zfill(1)+'d '+str(int(hr)).zfill(1)+'hr'
+
+
+
+   if remesh_elastic == False:
+
+       Vdg = FunctionSpace(model.mesh.mesh, 'DG',1)
+       Vcg = FunctionSpace(model.mesh.mesh, 'DG',1)
+       (xp , pstrain , ptemp, pepsII) = (p. return_property(mesh , 0) ,
+          p. return_property(mesh , 1) ,
+          p. return_property(mesh , 2),
+          p. return_property(mesh , 3))
+       del p
+       p = particles(xp, [pstrain,ptemp,pepsII], model.mesh.mesh)
+   else:
+       p.relocate()
+
+   # Advect particles -- Turn this on to advect particles now that it is removed from Stokes script
+   Vdg = FunctionSpace(model.mesh.mesh, 'DG',1)
+   ap = advect_rk3(p, model.vector2, model.u_k, "open")
+   ap.do_step(time_step)
+   AD = AddDelete(p, p_min, p_max, [interpolate(model.strain,Vdg), interpolate(model.temp,Vdg) , interpolate(model.epsII,Vdg)]) # Sweep over mesh to delete/insert particles
+   AD.do_sweep()
+
+   # Plotting
+   (xp , pstrain , ptemp, pepsII) = (p. return_property(mesh , 0) ,
+       p. return_property(mesh , 1) ,
+       p. return_property(mesh , 2),
+       p. return_property(mesh , 3))
+   pstrain[xp[:,0]<xyield_min]=0.0
+   p.change_property(pstrain,1)
+
+
+   xx=np.linspace(0,length*1.5,101)
+   xs=np.linspace(0,length,101)
+
+   plt.figure(1);plt.clf();
+   plt.subplot(2,2,1)
+   c=plot(u);plt.colorbar(c)
+   ax=plt.subplot(2,2,2)
+   c=plt.scatter(xp[:,0],xp[:,1],s=1.0,c=np.log10(pepsII/material.time_factor+1e-16),vmin=-8,vmax=-5);cbar2=plt.colorbar(c);plt.axis('equal')
+   #plt.xlim([0,length])
+   #plt.ylim([0,length])
+   ax.set_aspect('equal',adjustable='box')
+
+   ax=plt.subplot(2,2,3)
+   c=plt.scatter(xp[:,0],xp[:,1],s=1.0,c=np.log10(np.maximum(pstrain,1e-16)),vmin=-4,vmax=1);cbar1=plt.colorbar(c);
+   #plt.xlim([0,length])
+   #plt.ylim([0,length])
+   ax.set_aspect('equal',adjustable='box')
+
+   ax=plt.subplot(2,2,4)
+   plt.plot(s,f1)
+   plt.plot(s,f2,'--')
+   #plt.xlim([])
+   plt.pause(1e-16);
+   print('Time step',time_step,'Stress',sigma11,'Shear',sigma12,'Percent yielded',glenVisc.num_yielded,'Maximum strain',np.max(pstrain))
+
+   # Print some diagnostics to screen for debugging purpose
+   t = t+time_step
+   print('*******************************************')
+   print('Time:  ',t*material.time_factor/material.secpera,'Time step',time_step)
+   print(np.max(u.compute_vertex_values()))
+   print('*******************************************')
+   if t>1.01:
+       break
+   if glenVisc.num_yielded>0.0:
+       strain_rate = strain_rate + time_step*10.0
+       model.right_vel = -strain_rate*(model.right_wall-model.left_wall)/2
+       model.left_vel =   strain_rate*(model.right_wall-model.left_wall)/2
+   else:
+       strain_rate = strain_rate + time_step*10.0
+       model.right_vel = -strain_rate*(model.right_wall-model.left_wall)/2
+       model.left_vel =   strain_rate*(model.right_wall-model.left_wall)/2
+   x=model.mesh.mesh.coordinates()
+   print('Right wall',model.right_wall,'Mesh wall',np.max(x[:,0]))
+
+   #strain_rate = strain_rate+ strain_rate_accel
+
+outF.close()
+#if model.method==1:
+#    plt.figure(4);plt.plot(tlist,L2_strain,label=time_step_secs)
+#else:
+#    plt.figure(4);plt.plot(tlist,L2_strain,'--',label=time_step_secs)
+#plt.legend()
